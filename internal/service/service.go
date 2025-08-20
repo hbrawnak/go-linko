60c4f56bd2f08bfb59539c414c319e01485368f9
Habibur Rahman <hbrawnak@gmail.com>
2025-08-20 12:15:57 +0600

    refactor: Clean up service package by moving utilities
    - Remove Base62 encoding functions (moved to utils package)
    - Remove validation constants and regex (moved to utils package)
    - Remove IsBase62 and IsLengthOk methods (moved to utils package)
    - Update GenerateShortCode to use utils.HashToBase62
    - Update GetShortCode to use utils.ShortCodeLenMin constant
    - Simplify service package to focus on core business logic

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,23 +1,13 @@
 package service
 
 import (
-	"crypto/sha256"
-	"encoding/binary"
 	"fmt"
 	"github.com/hbrawnak/go-linko/internal/data"
 	"github.com/hbrawnak/go-linko/internal/database"
+	"github.com/hbrawnak/go-linko/internal/utils"
 	"log"
-	"regexp"
 	"time"
 )
-
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-var base62Regex = regexp.MustCompile("^[a-zA-Z0-9]+$")
-
-const shortCodeLenMin = 7
-const shortCodeLenMax = 8
-const redisTTL = 24 * time.Hour
 
 type Service struct {
 	Models data.Models
@@ -26,51 +16,13 @@ type Service struct {
 
 func (s *Service) GenerateShortCode() string {
 	code := s.GetShortCode()
-	return hashToBase62(code)
+	return utils.HashToBase62(code)
 }
 
 func (s *Service) GetShortCode() string {
 	incr := s.Redis.INCR()
-	padded := fmt.Sprintf("%0*d", shortCodeLenMin, incr)
+	padded := fmt.Sprintf("%0*d", utils.ShortCodeLenMin, incr)
 	return padded
-}
-
-func ToBase62(num uint64) string {
-	if num == 0 {
-		return string(letters[0])
-	}
-	var encoded []byte
-	for num > 0 {
-		remainder := num % 62
-		encoded = append([]byte{letters[remainder]}, encoded...)
-		num = num / 62
-	}
-	return string(encoded)
-}
-
-func hashToBase62(input string) string {
-	hash := sha256.Sum256([]byte(input))
-
-	num := binary.BigEndian.Uint64(hash[:8])
-	code := ToBase62(num)
-
-	for len(code) < shortCodeLenMin {
-		code = "0" + code
-	}
-
-	if len(code) > shortCodeLenMax {
-		code = code[:shortCodeLenMax]
-	}
-
-	return code
-}
-
-func (s *Service) IsBase62(code string) bool {
-	return base62Regex.MatchString(code)
-}
-
-func (s *Service) IsLengthOk(code string) bool {
-	return len(code) >= shortCodeLenMin && len(code) <= shortCodeLenMax
 }
 
 func (s *Service) UpdateHitCountBG(c string) {
